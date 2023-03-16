7d464d4e46a4c8dabaffdfb6e6b9ec6babd2a204
joshpoduska <joshpoduska@gmail.com>
2023-03-16 12:24:17 -0700

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,11 @@
+mkdir ~/.streamlit
+echo "[browser]" > ~/.streamlit/config.toml
+echo "gatherUsageStats = true" >> ~/.streamlit/config.toml
+echo "serverAddress = \"0.0.0.0\"" >> ~/.streamlit/config.toml
+echo "serverPort = 8888" >> ~/.streamlit/config.toml
+echo "[server]" >> ~/.streamlit/config.toml
+echo "port = 8888" >> ~/.streamlit/config.toml
+echo "enableCORS = false" >> ~/.streamlit/config.toml
+echo "enableXsrfProtection = false" >> ~/.streamlit/config.toml
+ 
+streamlit run streamlit_app.py
